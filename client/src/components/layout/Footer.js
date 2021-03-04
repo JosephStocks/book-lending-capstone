d8b615134c51c3b51b87b57946cd978e2f457ded
Jacob Deel <jacoblakedeel@gmail.com>
2021-03-04 11:41:04 -0500

    Updated NavBar and Footer

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Navbar, Container } from 'react-bootstrap';
+import {Link} from 'react-router-dom';
 
 const Footer = () => {
   return (
     <>
-      <div>Footer</div>
+      <Navbar fixed="bottom" expand="lg" variant="light" bg="info">
+        <Container>
+          <Navbar.Brand as={Link} to="/"></Navbar.Brand>
+        </Container>
+      </Navbar>
     </>
   );
 };
