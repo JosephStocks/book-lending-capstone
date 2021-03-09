a7ac295e9658a6a5558d82c021bcedcc04bd1e25
Jacob Deel <jacoblakedeel@gmail.com>
2021-03-09 09:19:31 -0500

    Set Up Personal Tab Conditionals

--- client/src/components/TabbedSections.js
+++ client/src/components/TabbedSections.js
@@ -1,32 +1,102 @@
-import React from "react";
+import React, {useState} from "react";
 import Tab from "react-bootstrap/Tab";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Nav from 'react-bootstrap/Nav';
 
 export default function TabbedSections() {
-  return (
-    <>
-      <Tab.Container id="left-tabs-example" defaultActiveKey="first">
-        <Row>
-          <Col>
-            <Nav variant="tabs" className="flex-row">
-              <Nav.Item>
-                <Nav.Link eventKey="first" >My Books</Nav.Link>
-              </Nav.Item>
 
-              <Nav.Item>
-                <Nav.Link eventKey="second">Books I've Read</Nav.Link>
-              </Nav.Item>
+  const [tabShowing, setTabShowing] = useState("My Books");
 
-              <Nav.Item>
-                <Nav.Link eventKey="third">Books To Read</Nav.Link>
-              </Nav.Item>
-            </Nav>
-          </Col>
-        </Row>
-      </Tab.Container>
+  const handleClick = (tabState) => {
+      setTabShowing(tabState)
+  }
+  
 
-    </>
-  );
+  if(tabShowing === "My Books"){
+
+    return (
+      <>
+        <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+          <Row>
+            <Col>
+              <Nav variant="tabs" className="flex-row">
+                <Nav.Item>
+                  <Nav.Link onClick={()=>handleClick("My Books")} eventKey="first" >My Books</Nav.Link>
+                </Nav.Item>
+  
+                <Nav.Item>
+                  <Nav.Link onClick={()=>handleClick("Read")} eventKey="second">Books I've Read</Nav.Link>
+                </Nav.Item>
+  
+                <Nav.Item>
+                  <Nav.Link onClick={()=>handleClick("To Read")} eventKey="third">Books To Read</Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Col>
+          </Row>
+        </Tab.Container>
+        <div>My Books</div>
+        
+  
+      </>
+    );
+
+  }
+  else if(tabShowing === "Read"){
+    return (
+      <>
+        <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+          <Row>
+            <Col>
+              <Nav variant="tabs" className="flex-row">
+                <Nav.Item>
+                  <Nav.Link onClick={()=>handleClick("My Books")} eventKey="first" >My Books</Nav.Link>
+                </Nav.Item>
+  
+                <Nav.Item>
+                  <Nav.Link onClick={()=>handleClick("Read")} eventKey="second">Books I've Read</Nav.Link>
+                </Nav.Item>
+  
+                <Nav.Item>
+                  <Nav.Link onClick={()=>handleClick("To Read")} eventKey="third">Books To Read</Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Col>
+          </Row>
+        </Tab.Container>
+        <div>Books I've Read</div>
+      </>
+    );
+  }
+  else if(tabShowing === "To Read"){
+    return (
+      <>
+        <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+          <Row>
+            <Col>
+              <Nav variant="tabs" className="flex-row">
+                <Nav.Item>
+                  <Nav.Link onClick={()=>handleClick("My Books")} eventKey="first" >My Books</Nav.Link>
+                </Nav.Item>
+  
+                <Nav.Item>
+                  <Nav.Link onClick={()=>handleClick("Read")} eventKey="second">Books I've Read</Nav.Link>
+                </Nav.Item>
+  
+                <Nav.Item>
+                  <Nav.Link onClick={()=>handleClick("To Read")} eventKey="third">Books To Read</Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Col>
+          </Row>
+        </Tab.Container>
+        <div>Books I Want to Read</div>
+        
+  
+      </>
+    );
+  }
+
+  
 }
