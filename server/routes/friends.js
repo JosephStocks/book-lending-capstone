88f44da34ccfe2847877b5382d1f901be3811713
Joseph Stocks <jpstocks63@gmail.com>
2021-03-11 13:38:00 -0600

    friends logic changes

--- server/routes/friends.js
+++ server/routes/friends.js
@@ -10,6 +10,21 @@ require("../auth/passAuth"); //import all of passport auth strategy
 // jwt auth
 let requireAuth = passport.authenticate("jwt", { session: false });
 
+router.post("/books", requireAuth, async (req, res) => {
+  let { book, whichList } = req.body;
+  let userID = req.user.id;
 
+  let response = await findOrCreateBookEntry(book);
+  console.log(Object.keys(response));
+  let bookID = response.dataValues.id;
+
+  response = await findOrCreatePersonalListFunctionMapping[whichList](
+    userID,
+    bookID
+  );
+
+  // console.log(response);
+  res.status(200).json(response);
+});
 
 module.exports = router;
