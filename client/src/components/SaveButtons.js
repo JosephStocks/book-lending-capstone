f39b7750e874d6cf74fdf86ac51092fd46b6d308
Ian Haddock <ianhaddock99@gmail.com>
2021-03-11 16:25:25 -0500

    deleted toast

--- client/src/components/SaveButtons.js
+++ client/src/components/SaveButtons.js
@@ -4,18 +4,13 @@ import * as S from "../styles/Styles";
 import {
   addBookToPersonalLists,
 } from "../api-calls/internal-api";
-import 'react-toastify/dist/ReactToastify.css';
 
 export default function SaveButtons() {
 
   const individBook = useSelector((state) => state.individBook); 
   const tokenFromState = useSelector((state) => state.token);
 
-  const notify = () => toast("Wow so easy!");
 
-
-
-  
 
   return (
     <>
