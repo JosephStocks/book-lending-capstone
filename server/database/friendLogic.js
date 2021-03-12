434638336850143fc05998ee737277ebbb5614bf
Joseph Stocks <jpstocks63@gmail.com>
2021-03-12 14:27:25 -0600

    fixed await error

--- server/database/friendLogic.js
+++ server/database/friendLogic.js
@@ -1,6 +1,61 @@
 const db = require("../models");
 const axios = require("axios");
 
-const dummyFunction = () => {};
+const fetchFriendsFromDatabase = async (userID) => {
+  return await db.friendsRelations.findAll({
+    where: {
+      userID: userID,
+    },
+    include: [
+      {
+        model: db.books,
+      },
+    ],
+    raw: true,
+  });
+};
 
-module.exports.dummyFunction = dummyFunction;
+const fetchUserFromDatabaseByLocalEmail = async (email) => {
+  return await db.user.findAll({
+    where: {
+      email: email,
+    },
+    raw: true,
+  });
+};
+
+const fetchUserFromDatabaseByGoogleAuthEmail = async (email) => {
+  return await db.user.findAll({
+    where: {
+      googleAuth: email,
+    },
+    raw: true,
+  });
+};
+
+const searchUsersFromDatabaseByNameOREmail = async (query) => {
+  return await db.user.findAll({
+    where: {
+      [Op.or]: {
+        namesQuery: sequelize.where(
+          sequelize.fn(
+            "concat",
+            sequelize.col("firstName"),
+            " ",
+            sequelize.col("lastName")
+          ),
+          {
+            [Op.ilike]: `%${query}%`,
+          }
+        ),
+        email: { [Op.ilike]: `%${query}%` },
+        googleAuth: { [Op.ilike]: `%${query}%` },
+      },
+    },
+  });
+};
+
+module.exports.fetchFriendsFromDatabase = fetchFriendsFromDatabase;
+module.exports.fetchUserFromDatabaseByLocalEmail = fetchUserFromDatabaseByLocalEmail;
+module.exports.fetchUserFromDatabaseByGoogleAuthEmail = fetchUserFromDatabaseByGoogleAuthEmail;
+module.exports.searchUsersFromDatabaseByNameOREmail = searchUsersFromDatabaseByNameOREmail;
