c62174f84800c4ea2ce537d42c89bab8f6bd1048
Joseph Stocks <jpstocks63@gmail.com>
2021-03-14 23:59:55 -0500

    added ability to see which users own a book - just the server route

--- server/routes/books.js
+++ server/routes/books.js
@@ -8,6 +8,7 @@ const {
   showAllWantToReadBooksByUser,
   findOrCreateBookEntry,
   findOrCreatePersonalListFunctionMapping,
+  showUsersWhoOwnBook,
 } = require("../database/bookLogic");
 // auth
 const passport = require("passport");
@@ -67,6 +68,15 @@ router.post("/books", requireAuth, async (req, res) => {
 router.delete("/books", async (req, res) => {
   try {
     let response = deleteBookByAnyID(req.body);
+    res.status(200).json(response);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+// requireAuth,
+router.post("/whoownsit", async (req, res) => {
+  try {
+    let response = await showUsersWhoOwnBook(req.body.bookID);
     res.status(200).json(response);
   } catch (err) {
     res.status(500).json({ message: err.message });
